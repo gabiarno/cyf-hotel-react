d14b8055b749f197cff83fb53086c5739b055867
gabiarno <gabiarno@hotmail.com>
2022-05-14 17:19:36 +0200

    homework
    homework

--- src/components/SearchResultsRow.js
+++ src/components/SearchResultsRow.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 
 const SearchResultsRow = props => {
   const checkIn = moment(props.row.checkInDate);
   const checkOut = moment(props.row.checkOutDate);
   const nights = checkOut.diff(checkIn, "days");
+
+  const [selected, setSelected] = useState("notSelected");
+  const selectRow = () => {
+    if (selected === "notSelected") {
+      setSelected("isSelected");
+    } else {
+      setSelected("notSelected");
+    }
+  };
   return (
-    <tr>
+    <tr className={selected} onClick={selectRow}>
       <td scope="row">{props.row.id}</td>
       <td>{props.row.title}</td>
       <td>{props.row.firstName}</td>
